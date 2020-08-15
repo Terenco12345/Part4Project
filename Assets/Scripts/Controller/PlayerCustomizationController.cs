83f004b9543fdf186fb77a178c3ba47aa8e1d87a
Terence Qu <Terenco12345@gmail.com>
2020-08-15 12:55:43 +1200

    Implemented basic model changing system
    - Added new model preset
    - Added system to change models through ModelManager and
    PlayerCustomizationController

---
+++ Assets/Scripts/Controller/PlayerCustomizationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerCustomizationController : NetworkBehaviour
+{
+    public PlayerBehaviour playerBehaviour;
+    
+    [Command]
+    public void CmdChangeSelection(int selection)
+    {
+        ModelManager.Instance.tileSetSelection = selection;
+    }
+}
